7f6924bc35fc3a7e15e0514a6d7ccd35231d5798
Evan Kooyer <echofish626@protonmail.com>
2024-02-11 20:30:51 -0600

    added Item class and placeholder for furniture class, as well as death and destroy item methods for respective classes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
 
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, location):
         self.name = name
         self.health = 100
         self.armor = 0
         self.inventory = []
-        self.equipped_item = self.inventory[0]
+        self.equipped_item = None
+        self.location = 
     
     def equip_item(self, selection):
         selection.lower()
@@ -18,14 +19,18 @@ class Person:
     def attack(self):
         pass
 
-    def grab_item(self):
-        pass
+    def grab_item(self, selection, room):
+        if type(room.selection) == type(Item):
+            self.inventory.append(selection)
 
     def search_area(self):
         pass
 
+    def die(self):
+        pass
+
 class Room:
-    def init(self, furniture = None, items = None, connected_doors = None):
+    def __init__(self, furniture = None, items = None, connected_doors = None):
         if furniture is None:
             furniture = []
         
@@ -39,3 +44,36 @@ class Room:
         self.items = items
         self.connected_doors = connected_doors
 
+class Item:
+    def __init__(self, name, hitpoints=0, defpoints=0, healfactor=0, consumable = True, num_uses = 1): 
+        self.name = name
+        self.damage = hitpoints
+        self.defense = defpoints
+        self.healfactor = healfactor
+        self.consumable = consumable
+
+        if self.consumable == True:
+            self.usecounter = num_uses
+        else:
+            self.usecounter = 999
+
+    def use_item(self, target):
+        if type(target) == type(Person):
+            target.health = target.health + self.healfactor - self.damage
+            if target.health <= 0:
+                target.die()
+        
+        if self.consumable == True:
+            self.usecounter -= 1
+            if self.usecounter <= 0:
+                self.destroy()
+    
+    def destroy(self):
+        pass
+
+class Furniture:
+    pass
+
+
+player1 = Person('Steve')
+enemy1 = Person('John')
